6a98adbf284c7b03dae7a11d9d9a2f999dd031d9
Jhana Barnes <jhanabarnes@gmail.com>
2022-10-15 01:05:26 -0500

    Add an X or O to a Square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,10 @@
 window.onload = ()=> {
     let board = document.getElementById("board");
     let squares = [...board.children];
+    let game_state = new Array(9).fill(" ");
+    var turn_tracker = 0
 
-    // Write the necessary JavaScript event-handling code so that when the page loads,
-    // each square in the 3x3 grid game board is styled appropriately:
-
-    squares.forEach((square) => {
+    squares.forEach( (square) => {
         square.classList.add('class','square');
 
         // Add Hover Effect
@@ -17,6 +16,22 @@ window.onload = ()=> {
         square.addEventListener('mouseout',() => {
             square.classList.remove('hover');
         });
+
+        // Add Alternating X's or O's to the Board
+        square.addEventListener('click', () => {
+            if (square.innerHTML === "") {
+                if (turn_tracker % 2 === 0) {
+                    square.innerHTML = "X";
+                    square.classList.add("square", "X");
+                    turn_tracker += 1;
+                } else {
+                    square.innerHTML = "O";
+                    square.classList.add("square", "O");
+                    turn_tracker += 1;
+                }
+            }
+        })
+
 
     })
 
